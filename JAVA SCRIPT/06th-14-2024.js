d18608d8484adcb7b68323e3bd06140db2e42f24
mukkara <rsr.mukkar45@gmail.com>
2024-06-15 05:52:32 +0530

    pending works related to html and css and taks

---
+++ JAVA SCRIPT/06th-14-2024.js
@@ -0,0 +1,28 @@
+console.log("Hi 1", a);
+console.log("Hi 2", abcd);
+
+
+
+
+// const a ="My Name 1";
+
+// console.log("a===>: 1", a);
+// a ="My Name 2";
+// console.log("a===>: 2", a);
+// //Hoisting
+var a;
+var abc = "Ranga";
+
+console.log("I'm: 1",abc);
+
+ var abc ="RSR";
+
+console.log("I'm: 2",abc)
+
+let abcd = "Ranga";
+
+console.log("I'm: 1",abcd);
+
+ abcd ="RSR";
+
+console.log("I'm: 2",abcd)
